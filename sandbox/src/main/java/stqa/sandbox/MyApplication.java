804b1dd183c7067946691ccaf169276cd0fe69e2
admin-max <erste1max@gmail.com>
2018-06-16 23:59:03 -0500

    Added couple functions

--- sandbox/src/main/java/stqa/sandbox/MyApplication.java
+++ sandbox/src/main/java/stqa/sandbox/MyApplication.java
@@ -4,7 +4,28 @@ class MyApplication {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("John");
+    hello("Alex");
 
+    double l = 5;
+    System.out.println("Square area with " + l + " side is: " + area(l));
+
+    double a = 3;
+    double b = 4;
+    System.out.println("Rectangle square area with " + a + " and " + b + " sides is: " + area(a, b));
+
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 }
