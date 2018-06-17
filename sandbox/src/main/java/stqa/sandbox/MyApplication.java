b8b6f55a625569989660c326cab14292ea5aaa06
admin-max <erste1max@gmail.com>
2018-06-16 23:19:39 -0500

    Alligned

--- sandbox/src/main/java/stqa/sandbox/MyApplication.java
+++ sandbox/src/main/java/stqa/sandbox/MyApplication.java
@@ -2,9 +2,9 @@ package stqa.sandbox;
 
 class MyApplication {
 
-public static void main (String [] args){
+  public static void main(String[] args) {
 
-System.out.println("Hello, world!");
+    System.out.println("Hello, world!");
 
-}
+  }
 }
